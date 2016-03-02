3805d91a96b4b70c0205fb15ee0ef31a8c9a7a16
micahstubbs <micah.stubbs@gmail.com>
2016-03-02 13:23:46 -0800

    add the ability to calculate abbreviations

--- src/extract-province-names-from-geojson.js
+++ src/extract-province-names-from-geojson.js
@@ -7,11 +7,11 @@ var json2csv = require('json2csv');
 var inputDir = __dirname + '/input';
 var outputDir = __dirname + '/output';
 
-var mapData = require(inputDir + '/vietnam.json');
+var mapData = require(inputDir + '/california.json');
 
 var provinces = mapData['features'].map(function(d) {
 	return {
-		'province': d['properties']['NAME_1']
+		'province': d['properties']['NAME']
 	};
 })
 
@@ -22,6 +22,82 @@ var fields = ['province'];
 json2csv({ data: provinces, fields: fields }, function(err, csv) {
   if (err) console.log(err);
   fs.writeFile('provinceNamesFromGeoJSON.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+  });
+});
+
+var provincesFlatArray = provinces.map(function(d) {
+	return d['province'];
+})
+
+// set default abbreviationLength
+var abbreviationLength = 2;
+
+// calculate short posssible abbreviation for the list of provinces
+function generateAbbreviations(array) {
+	var lengths = array.map(function(d) { return d.length; })
+	var minLength = d3.min(lengths);
+	var abbreviations;
+
+	for(var i=0; i<minLength; i++) {
+		abbreviations = array.map(function(d) { 
+			d = d.replace(/\s/g, "");
+			abbreviationLength = i+1;
+			return d.substring(0, i+1) 
+		})
+		abbreviations = _.uniq(abbreviations);
+		if(abbreviations.length === array.length) {
+
+			break
+		}
+	}
+	return abbreviations;
+} 
+
+var abbreviationsFlatArray = generateAbbreviations(provincesFlatArray);
+var abbreviations = abbreviationsFlatArray.map(function(d) {
+	return {
+		'abbreviation': d
+	}
+})
+console.log('abbreviations', abbreviationsFlatArray);
+console.log('abbreviationLength', abbreviationLength);
+
+var fields = ['abbreviation'];
+
+json2csv({ data: abbreviations, fields: fields }, function(err, csv) {
+  if (err) console.log(err);
+  fs.writeFile('provinceAbbreviationsFromGeoJSON.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+  });
+});
+
+// output format:
+// enName,enAbbr,localName,localAbbr
+var abbreviation;
+var provincesOutput = provincesFlatArray.map(function(d) {
+
+	abbreviation = d.replace(/\s/g, "").substring(0, abbreviationLength) 
+
+	return {
+		'enName': d,
+		'enAbbr' : abbreviation,
+		'localName': d,
+		'localAbbr': abbreviation
+	};
+})
+
+
+
+//console.log('provinces', provinces);
+
+var fields = ['enName', 'enAbbr', 'localName', 'localAbbr'];
+
+json2csv({ data: provincesOutput, fields: fields, quotes: false }, function(err, csv) {
+  if (err) console.log(err);
+  fs.writeFile(inputDir + '/' + 'provinces.csv', csv, function(err) {
     if (err) throw err;
     console.log('file saved');
   });
